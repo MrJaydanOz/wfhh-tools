60a139c86cc21696ecc841f0a6d27c016c7167a9
MrJaydanOz <jaydan@jaysanlunt.com.au>
2024-10-29 10:17:20 +1100

    Test Two Page

--- src/timed-counter.tsx
+++ src/timed-counter.tsx
@@ -5,12 +5,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <h1>Select Tool</h1>
-        <div style={{ margin: "", flexDirection: "row" }}>
-            <a href="timed-counter.html" style={{ minWidth: "max-content", height: "48pt", background: "no-repeat padding-box linear-gradient(#22e6d3, #0092aa)" }} className="button">Timed Counter</a>
-        </div>
+        <h1>Timed Counter</h1>
     </React.StrictMode>
 );
+
+(document.querySelector("title") as HTMLTitleElement).textContent = "WFHH Tools"
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
